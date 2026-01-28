998d15091e6953b6162adc6f834e147b9bd8b5dc
HM-Tanjil9 <rtajil0@gmail.com>
2026-01-28 23:23:10 +0600

    added short-term memory in AI chat

--- backend/src/sockets/socket.server.js
+++ backend/src/sockets/socket.server.js
@@ -3,6 +3,7 @@ const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
 const aiService = require("../service/ai.service");
+const messageModel = require("../models/messages.model");
 
 function initSocketServer(httpServer) {
     const io = new Server(httpServer, {});
@@ -24,20 +25,38 @@ function initSocketServer(httpServer) {
     io.on("connection", (socket) => {
         console.log("A user connected:", socket.user);
         console.log("New socket connection:", socket.id);
-        socket.on('ai-message', (messagePayload) => {
+        socket.on('ai-message',async (messagePayload) => {
             console.log("message payload:", messagePayload);
+            await messageModel.create({
+                chat: messagePayload.chat,
+                user: socket.user._id,
+                content: messagePayload.content,
+                role: 'user'
+            });
+
+            const chatHistory = await messageModel.find({
+                chat: messagePayload.chat
+            });
+            console.log("chat history", chatHistory.map(item => {
+                return {
+                    role: item.role,
+                    parts: [{text: item.content}]
+                }
+            }));
             
-            aiService.generateResponse(messagePayload.content)
-                .then(response => {
-                    socket.emit('ai-response', { 
-                        content: response,
-                        chat: messagePayload.chat
-                     });
-                })
-                .catch(err => {
-                    console.error("AI service error:", err);
-                    socket.emit('ai-response', { content: "Error generating response."   });
-                }); 
+            const response =await aiService.generateResponse(messagePayload.content);
+            
+            await messageModel.create({
+                chat: messagePayload.chat,
+                user: socket.user._id,
+                content: response,
+                role: 'model'
+            });
+
+            socket.emit('ai-response', { 
+                content: response,
+                chat: messagePayload.chat
+            });
         });
 
         socket.on("disconnect", () => {
